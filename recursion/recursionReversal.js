172a486063a1f9b066b754209f72653bb1a97520
Nakul Bajaj <bajaj.nakul@gmail.com>
2025-02-05 00:28:24 +1100

    updated recursions

--- recursion/recursionReversal.js
+++ recursion/recursionReversal.js
@@ -9,14 +9,3 @@ function reverse(val){
 
     return arr[arr.length - 1] + reverse(val.substring(0, arr.length - 1));
 }
-const basket = [4,1,2]
-basket.sort(function(a,b){
-    console.log(a,b)
-    console.log("Next")
-;    return a-b
-});
-
-console.log(basket);
-
-//3, -2, -2, 1
-
